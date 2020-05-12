c3bfae4148cc5395b0819c44a45f88e238820dab
Garrett James <garrettjames55@gmail.com>
2020-05-12 10:02:18 -0400

    updated bookmarks server

--- src/app.js
+++ src/app.js
@@ -1,47 +1,30 @@
-/*******************************************************************
-  IMPORTS
-*******************************************************************/
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const validateBearerToken = require("./validateBearerToken");
-const errorHandler = require("./errorHandler");
-const bookmarksRouter = require("./bookmarks-router");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
+const bookmarksRouter = require("./bookmarks/bookmarks-router");
 
-/*******************************************************************
-  INIT
-*******************************************************************/
 const app = express();
 
-/*******************************************************************
-  MIDDLEWARE
-*******************************************************************/
-app.use(morgan(NODE_ENV === "production" ? "tiny" : "common"));
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  })
+);
 app.use(cors());
 app.use(helmet());
-//app.use(express.json()); //parses JSON data of req body
 app.use(validateBearerToken);
 
-/*******************************************************************
-  ROUTES
-*******************************************************************/
 app.use(bookmarksRouter);
 
-/*******************************************************************
-  ERROR HANDLING
-*******************************************************************/
-// Catch-all 404 handler
-app.use((req, res, next) => {
-  const err = new Error("Path Not Found");
-  err.status = 404;
-  next(err); // goes to errorHandler
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
+
 app.use(errorHandler);
 
-/*******************************************************************
-  EXPORTS
-*******************************************************************/
 module.exports = app;
